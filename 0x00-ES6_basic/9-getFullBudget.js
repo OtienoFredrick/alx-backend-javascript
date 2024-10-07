eb1e632955761bec03f6d6118f3ccc981265ccb0
marshall235 <fredie1738@gmail.com>
2024-10-07 19:20:14 +0300

    Fixing the code structure

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,10 +1,11 @@
+/* eslint-disable import/extensions */
 import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars (income) {
+    getIncomeInDollars(income) {
       return `$${income}`;
     },
     getIncomeInEuros(income) {
